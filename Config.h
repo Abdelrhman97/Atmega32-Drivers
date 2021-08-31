869399b1915a134377de0895903fd87e6a66762f
Abdelrhman shaban <51994083+Abdelrhman97@users.noreply.github.com>
2021-08-31 04:08:15 +0200

    Add files via upload

--- Config.h
+++ Config.h
@@ -9,29 +9,13 @@
 #define HIGH 1 
 #define LOW 0 
 
-#define OUTPUT 1 
-#define INPUT 0 
+typedef enum
+{
+	INPUT,
+	OUTPUT
+}DirState;
 
 /* ----- DIO Registers ---- */ 
-
-/*
-#define DDRA      *(volatile(u8* const)0x3A)
-#define PORTA	  *(volatile(u8* const)0x3B)
-#define PINA	  *(volatile(u8* const)0x39) 
-
-#define DDRB      *(volatile(u8* const)0x37)
-#define PORTB     *(volatile(u8* const)0x38)
-#define PINB      *(volatile(u8* const)0x36)
-
-#define DDRC      *(volatile(u8* const)0x34)
-#define PORTC     *(volatile(u8* const)0x35)
-#define PINC      *(volatile(u8* const)0x33)
-
-#define DDRD      *(volatile(u8* const)0x31)
-#define PORTD     *(volatile(u8* const)0x32)
-#define PIND      *(volatile(u8* const)0x30)
- 
-*/
 
 #define PORTA  *((volatile u8* const)0X3B)
 #define DDRA   *((volatile u8* const)0X3A)
